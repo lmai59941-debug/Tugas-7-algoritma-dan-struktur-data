4b59eb48a3fd4ed15022135955e2e9ce198f789b
Mailan <lmai59941@gmail.com>
2025-12-16 17:57:08 +0700

    Add files via upload

---
+++ nama kota.py
@@ -0,0 +1,15 @@
+listkota = [
+'Jakarta', 'Surabaya', 'Depok', 'Bekasi', 'Solo', 'Jogjakarta', 'Semarang', 'Makassar,'
+]
+kotaYangDicari = input('ketik nama kota yang kamu cari:')
+for i, kota in enumerate(listkota):
+      # kita ubah katanya ke lowercase agar 
+      # menjadi case insensitive
+      if kota.lower() == kotaYangDicari.lower():
+       print("kota yang anda cari berada pada indeks", i) 
+       break
+else:
+      print('Maaf,kota yang anda cari tidak ada')
+        
+        
+     
